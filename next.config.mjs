93ffd3e796a65dcc15a149bd2f9a048df491a5b1
meganl1e <m3ganl1e@gmail.com>
2025-05-30 16:20:51 -0700

    robots txt and sitemap

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: "https://proud-whisper-861c52bc8d.strapiapp.com/api/strapi-5-sitemap-plugin/sitemap.xml",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
