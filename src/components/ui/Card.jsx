4e3b5b571249c0a133016da379c3c5b609910df4
meganl1e <m3ganl1e@gmail.com>
2025-07-22 14:05:16 -0700

    remove card.jsx

--- src/components/ui/Card.jsx
+++
@@ -1,7 +0,0 @@
-export default function Card({ children, className = "" }) {
-  return (
-    <div className={`bg-white border border-gray-200 rounded-lg shadow p-6 mb-4 ${className}`}>
-      {children}
-    </div>
-  );
-} 
