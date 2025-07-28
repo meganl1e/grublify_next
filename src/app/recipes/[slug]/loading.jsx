eb882db81f49aff720664397c1d0f0267465463c
meganl1e <m3ganl1e@gmail.com>
2025-07-28 16:57:30 -0700

    recipe skeleton loading centered

--- src/app/recipes/[slug]/loading.jsx
+++ src/app/recipes/[slug]/loading.jsx
@@ -4,8 +4,8 @@ import "react-loading-skeleton/dist/skeleton.css";
 export default function Loading() {
   return (
     <div className="min-h-screen p-4 sm:p-8">
-      <div className="container mx-auto grid grid-cols-1 lg:grid-cols-12 gap-8 max-w-full lg:max-w-6xl">
-        <main className="w-full lg:col-span-6 lg:col-start-3 pt-4 lg:pt-6 space-y-6">
+      <div className="container mx-auto max-w-6xl">
+        <main className="w-full lg:w-1/2 lg:mx-auto pt-4 lg:pt-6 space-y-6">
           <div className="flex flex-col gap-1">
             <h3 className="text-lg font-bold text-secondary/80">
               <Skeleton width={64} />
@@ -17,7 +17,7 @@ export default function Loading() {
           <Skeleton height={400} width={400} className="rounded-2xl" />
           <Skeleton className="mt-6" height={200} width="100%" borderRadius={8} />
         </main>
-        <aside className="hidden lg:block p-6"></aside>
+
       </div>
     </div>
   );
