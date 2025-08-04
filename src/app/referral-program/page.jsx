16732a4cca6351fdc45fd731a2bead4f5616c2af
meganl1e <m3ganl1e@gmail.com>
2025-08-04 14:48:14 -0700

    new referral link

--- src/app/referral-program/page.jsx
+++ src/app/referral-program/page.jsx
@@ -5,9 +5,13 @@ export default function ReferralProgram() {
   return (
     <div className="flex justify-center items-center min-h-screen">
       <Button size="lg">
-        <Link href="https://docs.google.com/document/d/127gR3VBQ_mjlzP8Qjf5ohRxaV0qyRUHDL5zVzEs6pFc/edit?usp=drive_link">
+      <a 
+        href="https://forms.gle/G4pcQ7rUf7fzksNf8" 
+        target="_blank" 
+        rel="noopener noreferrer"
+      >
           Click for Referral Program Details
-        </Link>
+        </a>
       </Button>
     </div>
   );
