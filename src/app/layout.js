2525b12fa0d0a7e9e793ce8b72b9f2dd0e7562e7
meganl1e <m3ganl1e@gmail.com>
2025-08-18 13:01:49 -0700

    moved google tag back to head bc cursor said so

--- src/app/layout.js
+++ src/app/layout.js
@@ -40,7 +40,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* <Script
+        <Script
           src="https://www.googletagmanager.com/gtag/js?id=AW-17424660572"
           strategy="afterInteractive"
         />
@@ -51,25 +51,14 @@ export default function RootLayout({ children }) {
             gtag('js', new Date());
             gtag('config', 'AW-17424660572');
           `}
-        </Script> */}
+        </Script>
       </head>
       {/* <GoogleTagManager gtmId="GTM-W7GD9KBX" /> */}
 
       <body suppressHydrationWarning={true}>
         <TrackPageView />
 
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17424660572"
-          strategy="afterInteractive"
-        />
-        <Script id="google-ads-tag" strategy="afterInteractive">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'AW-17424660572');
-        `}
-        </Script>
+
         <Providers>
           <SkeletonTheme
             baseColor="rgba(120,130,140,0.18)"
